f3581391da670e7fd7f2a941e408dd1ce2030472
Mr Drew Westcott <drew.westcott@me.com>
2016-05-12 21:57:27 +0100

    Initial Commit

---
+++ FocusMinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FocusMinder
+//
+//  Created by Drew Westcott on 12/05/2016.
+//  Copyright © 2016 Drew Westcott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
